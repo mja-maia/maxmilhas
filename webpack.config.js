5e4f334a978db47044d2adcd25f937e472bd63a7
Miguel Maia <miguel@krint.io>
2019-01-24 23:24:46 -0200

    initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.jsx',
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        }]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/public',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+     plugins: [
+         new webpack.HotModuleReplacementPlugin()
+     ],
+    devServer: {
+        contentBase: './public',
+        hot: true
+    }
+};
